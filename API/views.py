d21f9ac6cadbe26f7bb1744b71db42a1364afba5
aziz jlassi <azizjlassi498@gmail.com>
2023-10-04 12:40:49 +0100

    first commit

---
+++ API/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from rest_framework import generics
+
+# Rest Framework Function-Based-View
+from rest_framework.decorators import api_view
+
+# Rest Framework Class-Based-View
+from rest_framework.views import APIView
+
+
+
+# Views here.
